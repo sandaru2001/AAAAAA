fde332132e788865f57f27a5a7d3c3fe998b3cfc
Sandaru Shehan <sandarushehan96@gmail.com>
2024-07-17 21:52:17 +0530

    Header Completed

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
+
+import Header from './Header'
+import Navigation from './Navigation'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <NavigationContainer>
+        <View style={styles.container}>
+            <StatusBar backgroundColor='#0e806a' /> 
+            <Header />
+
+            <View style={{ flex: 1, backgroundColor: '#ffff'}}>
+                <Navigation />
+            </View>
+        </View>
+    </NavigationContainer>
+  );
 }
 
 export default App
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
--- app/Header.tsx
+++ app/Header.tsx
@@ -1,10 +1,18 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { Feather, Fontisto, MaterialCommunityIcons } from '@expo/vector-icons'
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.container}>
+        <View style={styles.headerContainer}>
+            <Text style={styles.headerText}>Whatsapp</Text>
+            <View style={styles.iconContainer}>
+                <Feather name='camera' size={24} color="white" style={styles.icon}/>
+                <Fontisto name='search' size={24} color="white" style={styles.icon}/>
+                <MaterialCommunityIcons name='dots-vertical' size={24} color="white" style={styles.icon}/>
+            </View>
+        </View>
     </View>
   )
 }
@@ -15,7 +23,26 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#ffff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        paddingTop: 80,
+        paddingBottom: 8,
     },
+    headerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginHorizontal: 16,
+    },
+    headerText: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: '500',
+    },
+    iconContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8
+    },
+    icon: {
+        marginLeft: 20
+    }
 });
