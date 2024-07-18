68c2e96d9c958c88f93be1179539686ec2e683b8
Sandaru Shehan <sandarushehan96@gmail.com>
2024-07-18 09:07:47 +0530

    chat done

--- app/data/chat.tsx
+++ app/data/chat.tsx
@@ -1,14 +1,61 @@
 const DATA = [
     {
         "id": 1,
-        "name": 'First Item',
+        "name": 'Nimna',
+        "photos": '@/assets/images/nimna.png',
+        "lastMessage": "I'm Nimna, what are you up to today?",
+        "time": "10:00",
+        "isOnline": true
     },
     {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
+        "id": 2,
+        "name": 'Amil',
+        "photos": '@/assets/images/amil.png',
+        "lastMessage": "I'm Nimna, what are you up to today?",
+        "time": "10:00",
+        "isOnline": true
     },
     {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
+        "id": 3,
+        "name": 'Nipun',
+        "photos": '@/assets/images/nipun.png',
+        "lastMessage": "I'm Nimna, what are you up to today?",
+        "time": "10:00",
+        "isOnline": true
     },
+    {
+        "id": 4,
+        "name": 'Nethpali',
+        "photos": '@/assets/images/nethpali.png',
+        "lastMessage": "I'm Nimna, what are you up to today?",
+        "time": "10:00",
+        "isOnline": true
+    },
+{
+        "id": 5,
+        "name": 'Rohitha',
+        "photos": '@/assets/images/rohitha.png',
+        "lastMessage": "I'm Nimna, what are you up to today?",
+        "time": "10:00",
+        "isOnline": true
+    },
+    {
+        "id": 6,
+        "name": 'Bineth',
+        "photos": '@/assets/images/bineth.png',
+        "lastMessage": "I'm Nimna, what are you up to today?",
+        "time": "10:00",
+        "isOnline": true
+    },
+    {
+        "id": 7,
+        "name": 'Anura',
+        "photos": '@/assets/images/anura.png',
+        "lastMessage": "I'm Nimna, what are you up to today?",
+        "time": "10:00",
+        "isOnline": true
+    },
+    
 ];
+
+export default DATA
--- app/screen/Chat.tsx
+++ app/screen/Chat.tsx
@@ -1,10 +1,39 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import DATA from '../data/chat'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 const Chat = () => {
+  const [chat, setData] = useState(DATA)
+
+  useEffect(() => {
+    setData(DATA)
+  })
   return (
     <View style={styles.container}>
-      <Text>Chat</Text>
+      {/* <FlatList 
+        data={chat}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({item}) => (
+          <View style={styles.ChatContainer}>
+            <Image source={item.photos} style={styles.image}/>
+            <View style={styles.ChatContent}>
+              <View style={styles.ChatHeader}>
+                <Text style={styles.ChatName}>{item.name}</Text>
+                <Text style={styles.ChatTime}>{item.name}</Text>
+              </View>
+            </View>
+          </View>
+        )}
+
+      /> */}
+
+        
+        <View style={styles.chat}>
+          <Image source={{uri:'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'}} width={50}  height={50}/>
+          <Text>Name</Text>
+        </View>
+
     </View>
   )
 }
@@ -12,10 +41,50 @@ const Chat = () => {
 export default Chat
 
 const styles = StyleSheet.create({
+  chat:{
+    flexDirection: 'row'    
+  },
+
+
+
+
     container: {
         flex: 1,
-        backgroundColor: '#ffff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        marginTop: 16,
     },
+    ChatContainer: {
+      flexDirection: 'row',
+      marginRight: 16,
+      marginLeft: 16
+    },
+    image: {
+      width: 50,
+      height: 50,
+      backgroundColor: '#128C7E',
+      borderRadius: 25,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    ChatContent: {
+      flex: 5,
+      borderBottomWidth: 0,
+      marginLeft: 16,
+      paddingBottom: 16,
+      marginBottom: 13
+    },
+    ChatHeader: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginBottom: 2,
+      marginTop: 4
+    },
+    ChatName: {
+      fontSize: 16,
+      fontWeight: 'bold'
+    },
+    ChatTime: {
+      fontSize: 12,
+      color: '#A0A09E'
+    }
 });
